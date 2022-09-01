eb6f521aa335bcf08f40e6c4e7c2c151a4cb8ab6
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-09-01 12:45:15 +0000

    outlined the compiler structure with a few header files.

---
+++ Source/Compiler.h
@@ -0,0 +1,83 @@
+/* MIT License (C) Genesis Contributors
+ * This file contains the source code for the process of compilation
+ * Genesis programming language broken down into the following components:
+ * tokenisation, parsing, semantic analysis. */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <map>
+
+
+struct Lexeme {
+    //This is the struct that represents encapsulated tokens:
+    string lexeme; //the text,
+    char type[5];  //the type of lexeme: identifier, number, string, an operator, keyword, etc.
+};
+
+//This class is used to represent relations among the tokens as a hierarchical model.   
+class SourceSchema {
+    public:
+        SourceSchema *parent;
+        list<SourceSchema*> children;
+        Lexeme *lexeme;
+        SourceSchema(SourceSchema *parent, Lexeme *lexeme) {
+            this->parent = parent;
+            this->lexeme = lexeme;
+        }
+        void addChild(SourceSchema *child) {
+            children.push_back(child);
+        }
+        void Show(int depth) {
+            for (int i = 0; i < depth; i++) {
+                cout << "  ";
+            }
+            cout << lexeme->lexeme << endl;
+            for (SourceSchema *child : children) {
+                child->Show(depth + 1);
+            }
+        }
+};
+
+list<string> splitByKeywords(string input, list<string> keywords) {
+    //Splits the input string into the list of keywords.
+    list<string> output;
+
+}
+map<string, string> generateTokens(char source[]) {
+    //This function tokenises the source code into a list of lexemes.
+    map<string, string> tokens;
+    //Algebraic Operators:
+    tokens["Add"] = "+";
+    tokens["Subtract"] = "-";
+    tokens["Multiply"] = "*";
+    tokens["Divide"] = "/";
+    tokens["Assign"] = "=";
+    tokens["Modulo"] = "%";
+    //Boolean Operators:
+    tokens["Equals"] = "==";
+    tokens["Not equals"] = "!=";
+    tokens["Less"] = "<";
+    tokens["Not more"] = "<=";
+    tokens["More"] = ">";
+    tokens["Not less"] = ">=";
+    //Equation Operators:
+    tokens["Add to"] = "+=";
+    tokens["Subtract from"] = "-=";
+    tokens["Multiply by"] = "*=";
+    tokens["Divide by"] = "/=";
+    tokens["Modulo by"] = "%=";
+    //Special symbols:
+    tokens["Opening "] = "(";
+    tokens[")"] = ")";
+    tokens["{"] = "{";
+    tokens["}"] = "}";
+    tokens[";"] = ";";
+    tokens["."] = ".";
+    tokens[","] = ",";
+    tokens["\""] = "\"";
+
+    
+    return tokens;
+}
+
--- Source/Startup.cpp
+++ Source/Startup.cpp
@@ -1,15 +1,18 @@
 /* MIT License (C) Genesis Contributors
- * This file contains the source code for the process of compilation
- * Genesis programming language broken down into the following components:
- * tokenisation, parsing, semantic analysis. */
+ * This file is the central roadmap for the whole compiler that 
+ * binds all components together. For more detailed description 
+ * take a look at Source/Compiler.h for the compiler implementation
+ * and Source/Genesis.h for the inner language's features parsing.*/
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <Compiler.h>
+#include <Genesis.h>
 using namespace std;
 
 int main(){
     string statement;
-    ifstream source("Samples/guess-number.gen");
+    ifstream source("Samples/code.gen");
     if (source.is_open()){
         cout << "File opened successfully" << endl;
         while (getline(source, statement)){
