9291a48a57d61a2e9b278e77282a6bc735b591ed
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-09-01 13:59:14 +0000

    traversed the tokens and described Genesis.h

--- Source/Compiler.h
+++ Source/Compiler.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <list>
 #include <map>
 
@@ -15,17 +16,18 @@ struct Lexeme {
     char type[5];  //the type of lexeme: identifier, number, string, an operator, keyword, etc.
 };
 
-//This class is used to represent relations among the tokens as a hierarchical model.   
-class SourceSchema {
+
+/// @brief The hierarchical tree model of the tokens. 
+class Block {
     public:
-        SourceSchema *parent;
-        list<SourceSchema*> children;
+        Block *parent;
+        list<Block*> children;
         Lexeme *lexeme;
-        SourceSchema(SourceSchema *parent, Lexeme *lexeme) {
+        Block(Block *parent, Lexeme *lexeme) {
             this->parent = parent;
             this->lexeme = lexeme;
         }
-        void addChild(SourceSchema *child) {
+        void addChild(Block *child) {
             children.push_back(child);
         }
         void Show(int depth) {
@@ -33,54 +35,72 @@ class SourceSchema {
                 cout << "  ";
             }
             cout << lexeme->lexeme << endl;
-            for (SourceSchema *child : children) {
+            for (Block *child : children) {
                 child->Show(depth + 1);
             }
         }
 };
 
-list<string> splitByKeywords(string input, list<string> keywords) {
-    //Splits the input string into the list of keywords.
-    list<string> output;
-
-}
-map<string, string> generateTokens(char source[]) {
-    //This function tokenises the source code into a list of lexemes.
+/// @brief Generates the standard tokens used in the language.
+/// @return the list of standard tokens.
+auto generateTokens() {
+    //This function tokenises the source code 
+    //into a list of predefined lexemes.
     map<string, string> tokens;
     //Algebraic Operators:
-    tokens["Add"]      = "+";
-    tokens["Subtract"] = "-";
-    tokens["Multiply"] = "*";
-    tokens["Divide"]   = "/";
-    tokens["Assign"]   = "=";
-    tokens["Modulo"]   = "%";
+    tokens["+"]  = "Add";
+    tokens["-"]  = "Subtract";
+    tokens["*"]  = "Multiply";
+    tokens["/"]  = "Divide";
+    tokens["="]  = "Assign";
+    tokens["%"]  = "Modulo";
+    tokens["^"]  = "Power";
+    tokens["\\"] = "Root";
     //Boolean Operators:
-    tokens["Equals"]     = "==";
-    tokens["Not equals"] = "!=";
-    tokens["Less"]       = "<";
-    tokens["Not more"]   = "<=";
-    tokens["More"]       = ">";
-    tokens["Not less"]   = ">=";
+    tokens["=="] = "Equals";
+    tokens["!="] = "NotEquals";
+    tokens["<"]  = "Less";
+    tokens["<="] = "Not more";
+    tokens[">"]  = "More";
+    tokens[">="] = "Not less";
     //Equation Operators:
-    tokens["Add to"]        = "+=";
-    tokens["Subtract from"] = "-=";
-    tokens["Multiply by"]   = "*=";
-    tokens["Divide by"]     = "/=";
-    tokens["Modulo by"]     = "%=";
-    tokens["Strict assign"] = ":=";
+    tokens["+="] = "Add to";
+    tokens["-="] = "Subtract from";
+    tokens["*="] = "Multiply by";
+    tokens["/="] = "Divide by";
+    tokens["%="] = "Modulo by";
+    tokens[":="] = "Strict assign";
     //Special symbols:
-    tokens["Round opening"]  = "(";
-    tokens["Round closing"]  = ")";
-    tokens["Curly opening"]  = "{";
-    tokens["Curly closing"]  = "}";
-    tokens["Square opening"] = "[";
-    tokens["Square closing"] = "]";
-    tokens["Colon"]          = ":";
-    tokens["Semocolon"]      = ";";
-    tokens["Dot"]            = ".";
-    tokens["Comma"]          = ",";
-    tokens["Backslash"]      = "\\";
+    tokens[")"]  = "Round opening";
+    tokens[")"]  = "Round closing";
+    tokens["{"]  = "Curly opening";
+    tokens["}"]  = "Curly closing";
+    tokens["["]  = "Square opening";
+    tokens["]"]  = "Square closing";
+    tokens[":"]  = "Colon";
+    tokens[";"]  = "Semicolon";
+    tokens["."]  = "Dot";
+    tokens[","]  = "Comma";
 
     return tokens;
+}
+
+list<string> splitIntoComponents(string input) {
+    //Splits the input string into the components
+    //that can be parsed as individual tokens.
+    const auto tokens = generateTokens();
+    Block primary = Block(NULL, NULL);
+    //Step 1. Cleanse the comments.
+    auto comments = input.find("//");
+    if (comments == NULL) comments = input.find("/*");
+
+}
+
+list<Lexeme> tokenise(string input) {
+    //This function tokenises the source code into a list of lexemes.
+    list<Lexeme> lexemes; //Below is only the reference to the primary tokens.
+    const map<string, string> tokens = generateTokens();
+    
+    return lexemes;
 }
 
---
+++ Source/Genesis.h
@@ -0,0 +1,4 @@
+/* MIT License (C) Genesis Contributors
+ * This file contains all the Genesis-specific implementation
+ * details and the way how they are parsed during the compilation. */
+#include <iostream>
