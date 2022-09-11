c38420c050ad02864e5be1f52661623b1c60c0a9
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-09-11 17:17:32 +0000

    outlined the preprocessor.

--- Source/Front-end/Preprocessor.cc
+++ Source/Front-end/Preprocessor.cc
@@ -6,5 +6,30 @@
  * them before they were defined. */
 #include <iostream>
 #include <fstream>
+#include <map>
+#include "./Exports.cc"
 
-namespace Compiler::Analysis::Preprocessor {};
+namespace Compiler::Analysis::Preprocessor {
+    /// @brief The preprocessor struct that represents the rows in the 
+    /// dependency table: name of the library, its path and type.
+    typedef struct DependencyRecord {
+        string title; //The title of the dependency to be imported.
+        string path; //The path where to look for the dependency.
+        string version; //The version of the dependency.
+        string type; //The type of the item: class, function, etc.
+        //TODO: implement polymorphic parameter for functions and class member listings.
+
+    } DependencyRecord;
+    //The list of all external libraries and their resources.
+    vector<string, DependencyRecord> libraries;
+    /// @brief Establishes the dependencies and fetches their items with
+    /// types, such as classes, functions, etc. for semantic analysis.
+    /// @param source 
+    void linkDependencies(string source){}
+    //The ID table containing the identifiers and their types.
+    map<string, TokenSort> identifiers;
+    /// @brief Looks through the file and fetches the top-level 
+    /// declarations such as classes, functions, and constants.
+    /// @param source 
+    void searchIdentifiers(string source) {}
+};
